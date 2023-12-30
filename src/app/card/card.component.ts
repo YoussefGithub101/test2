c085ec023984e8b12b40b1fa94108572503b63c5
YoussefGitHub101 <yousseffredy10@gmail.com>
2023-12-30 11:05:04 +0200

    for test

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,10 +3,11 @@ import { ButtonModule } from 'primeng/button';
 import {ClipboardModule} from '@angular/cdk/clipboard';
 import { QRCodeModule } from 'angularx-qrcode';
 import { TooltipModule } from 'primeng/tooltip';
+import { MessagesModule } from 'primeng/messages';
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [ClipboardModule , QRCodeModule , ButtonModule,TooltipModule],
+  imports: [ClipboardModule , QRCodeModule , ButtonModule,TooltipModule,MessagesModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
